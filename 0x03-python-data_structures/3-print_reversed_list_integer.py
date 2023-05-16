937bba6bf427f494c760a5959e86b5914a54c793
souky7la <soukylahmidi@gmail.com>
2023-05-16 15:26:32 -0700

    committing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,11 +3,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        pass
-    else:
-       num = len(my_list) - 1
-
-       while num >= 0:
-            print("{:d}".format(my_list[num]))
-            num -= 1
+if my_list is None:
+        return
+    for i in reversed(my_list):
+        print('{:d}'.format(i))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,11 +3,8 @@
 
 
 def new_in_list(my_list, idx, element):
-    length = len(my_list)
-
-    copy_list = my_list[:]
-
-    if 0 <= idx < length:
-        copy_list[idx] = element
-
-    return(copy_list)
+ tmp_list = my_list[:]
+    if 0 <= idx < len(my_list):
+        tmp_list[idx] = element
+        return(tmp_list)
+    return(my_list)
