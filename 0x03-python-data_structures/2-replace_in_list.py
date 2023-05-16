f90335117db7e355a0e9ccd1060f4cfa72b2f725
souky7la <soukylahmidi@gmail.com>
2023-05-16 15:38:36 -0700

    committing

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A function that replaces an element of a list in a specific position"""
-
-
 def replace_in_list(my_list, idx, element):
-     if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return(my_list)
+    """Replace an element of a list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""a function that replaces an element in a list at a specific position without modifying the original list"""
-
-
 def new_in_list(my_list, idx, element):
- tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element
-        return(tmp_list)
-    return(my_list)
+    """Replace an element at specific index without modifying the whole list"""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
